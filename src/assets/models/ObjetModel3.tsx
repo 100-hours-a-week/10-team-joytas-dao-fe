80d408d589b4c207838d4c326bacabd5d304c236
erica0321 <hyunahrho29@gmail.com>
2024-08-14 14:51:37 +0900

    feat: 오브제 모델 추가

---
+++ src/assets/models/ObjetModel3.tsx
@@ -0,0 +1,36 @@
+import { useGLTF } from '@react-three/drei'
+import { GroupProps } from '@react-three/fiber'
+import { Mesh, Material } from 'three'
+
+interface GLTFResultExtended {
+  nodes: {
+    Object_2: Mesh
+    Object_3: Mesh
+  }
+  materials: {
+    material_0: Material
+  }
+}
+
+export function ObjetModel3(props: GroupProps) {
+  const { nodes, materials } = useGLTF(
+    '/models/objet_model3/scene.gltf'
+  ) as unknown as GLTFResultExtended
+
+  return (
+    <group {...props} dispose={null}>
+      <group rotation={[-2.384, 0.785, -0.702]}>
+        <mesh
+          geometry={nodes.Object_2.geometry}
+          material={materials.material_0}
+        />
+        <mesh
+          geometry={nodes.Object_3.geometry}
+          material={materials.material_0}
+        />
+      </group>
+    </group>
+  )
+}
+
+useGLTF.preload('/models/objet_model3/scene.gltf')
