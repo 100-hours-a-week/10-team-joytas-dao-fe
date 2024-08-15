0d9f411f2984165f19af106981f45cbfe9540b27
erica0321 <hyunahrho29@gmail.com>
2024-08-15 23:24:52 +0900

    feat: 오브제 프리뷰 컴포넌트 메인에 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,13 +7,8 @@ import {
   Banner,
   MyObjetContainer,
   MyObjetTitle,
-  ObjetContainer,
-  ObjetDescription,
-  ObjetImage,
-  ObjetTitle,
-  ObjetContent,
-  ObjetList,
 } from './HomeStyles'
+import ObjetPreview from '../../components/objet/ObjetPreview'
 
 export default function Home() {
   const name = 'JunPark'
@@ -27,56 +22,9 @@ export default function Home() {
         <Banner>광고</Banner>
         <MyObjetContainer>
           <MyObjetTitle>👀 나에 대한 오브제가 만들어졌어요!</MyObjetTitle>
-          <MyObjets />
+          <ObjetPreview />
         </MyObjetContainer>
       </GloablContainer16>
     </Layout>
-  )
-}
-
-function MyObjets() {
-  return (
-    <ObjetList>
-      <Objet
-        image='https://picsum.photos/id/237/200/300'
-        title='오브제 1'
-        description='오브제 1의 설명'
-      />
-      <Objet
-        image='https://picsum.photos/id/238/200/300'
-        title='오브제 2'
-        description='오브제 2의 설명'
-      />
-      <Objet
-        image='https://picsum.photos/id/239/200/300'
-        title='오브제 3'
-        description='오브제 3의 설명'
-      />
-      <Objet
-        image='https://picsum.photos/id/240/200/300'
-        title='오브제 4'
-        description='오브제 4의 설명'
-      />
-    </ObjetList>
-  )
-}
-
-function Objet({
-  image,
-  title,
-  description,
-}: {
-  image: string
-  title: string
-  description: string
-}) {
-  return (
-    <ObjetContainer>
-      <ObjetImage src={image} alt={title} />
-      <ObjetContent>
-        <ObjetTitle>{title}</ObjetTitle>
-        <ObjetDescription>{description}</ObjetDescription>
-      </ObjetContent>
-    </ObjetContainer>
   )
 }
