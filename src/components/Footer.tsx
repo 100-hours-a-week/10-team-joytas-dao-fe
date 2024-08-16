eb552e6404276448cde8b1432790177b7640e0fc
erica0321 <hyunahrho29@gmail.com>
2024-08-17 00:30:03 +0900

    feat: 메뉴바 아이콘 변경

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,30 @@
 import { FooterDiv, IconDiv, IconImg, IconText } from './LayoutStyle'
-import home from '../assets/images/home.png'
-import rocket from '../assets/images/rocket.png'
-import users from '../assets/images/users.png'
+import home from '../assets/images/footer/home.png'
+import rocket from '../assets/images/footer/rocket.png'
+import users from '../assets/images/footer/users.png'
 import profile from '../assets/images/profile.png'
-import { useNavigate } from 'react-router-dom'
+import homePurple from '../assets/images/footer/homePurple.png'
+import rocketPurple from '../assets/images/footer/rocketPurple.png'
+import usersPurple from '../assets/images/footer/usersPurple.png'
+import { useNavigate, useLocation } from 'react-router-dom'
 import { URL } from '../static'
 
 export default function Footer() {
   const navigate = useNavigate()
+  const pathname = useLocation().pathname
 
   return (
     <FooterDiv>
       <IconDiv onClick={() => navigate(URL.main)}>
-        <IconImg src={home} />
+        <IconImg src={pathname.includes('main') ? homePurple : home} />
         <IconText>홈</IconText>
       </IconDiv>
       <IconDiv onClick={() => navigate(URL.lounge)}>
-        <IconImg src={rocket} />
+        <IconImg src={pathname.includes('lounge') ? rocketPurple : rocket} />
         <IconText>라운지</IconText>
       </IconDiv>
       <IconDiv onClick={() => navigate(URL.users)}>
-        <IconImg src={users} />
+        <IconImg src={pathname.includes('users') ? usersPurple : users} />
         <IconText>유저</IconText>
       </IconDiv>
       <IconDiv onClick={() => navigate(URL.myRoom)}>
