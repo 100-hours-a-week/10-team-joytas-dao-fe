a6084394b8707aa5ba3e329b9cd2457f454f596c
erica0321 <hyunahrho29@gmail.com>
2024-08-18 23:55:29 +0900

    feat: 오브제 통화 기능 추가

---
+++ src/components/call/Video.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useRef, useState } from 'react'
+import { Container, VideoContainer, UserLabel } from './VideoStyle'
+
+interface Props {
+  email: string
+  stream: MediaStream
+  muted?: boolean
+}
+
+const Video = ({ email, stream, muted }: Props) => {
+  const ref = useRef<HTMLVideoElement>(null)
+  const [isMuted, setIsMuted] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (ref.current) ref.current.srcObject = stream
+    if (muted) setIsMuted(muted)
+  }, [stream, muted])
+
+  return (
+    <Container>
+      <VideoContainer ref={ref} muted={isMuted} autoPlay controls />
+      <UserLabel>{email}</UserLabel>
+    </Container>
+  )
+}
+
+export default Video
