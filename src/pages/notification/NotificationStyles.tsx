412f8edbcb1c23daeda83fed0ff25f6315930460
yoouung <jyp101400@gmail.com>
2024-08-19 14:14:48 +0900

    feat: 알림 목록 페이지 추가

---
+++ src/pages/notification/NotificationStyles.tsx
@@ -0,0 +1,79 @@
+import styled from 'styled-components'
+
+export const NotificationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: calc(100% - 35px);
+  overflow-y: auto;
+  box-sizing: border-box;
+  margin-top: 30px;
+`
+
+export const NotificationGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+`
+
+export const NotificationDate = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 75px;
+  height: 27px;
+  background-color: #423e3e;
+  border-radius: 30px;
+  box-sizing: border-box;
+  align-self: center;
+
+  font-size: 12px;
+  color: #d9d9d9;
+  word-spacing: -0.2px;
+  margin: 10px 0;
+`
+
+export const NotificationItemContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  width: 100%;
+`
+
+export const StyledHr = styled.hr`
+  border: 0.1px solid #666666;
+  width: 315px;
+`
+
+export const ProfileImg = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  background-color: white;
+`
+
+export const NotificationContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 250px;
+  gap: 2px;
+`
+
+export const NotiTitle = styled.span`
+  font-size: 11px;
+  font-weight: bold;
+  white-space: break-spaces;
+`
+
+export const NotiContents = styled.span`
+  font-size: 10px;
+  white-space: break-spaces;
+`
+
+export const NotiDatetime = styled.span`
+  font-size: 8px;
+  color: #6a6a6a;
+`
