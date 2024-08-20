f8ff25f429af90d52ec549fcf5e73672b3076208
erica0321 <hyunahrho29@gmail.com>
2024-08-20 18:52:52 +0900

    feat: 라운지 상세페이지 api 연결

--- src/pages/lounge/LoungeObjets.tsx
+++ src/pages/lounge/LoungeObjets.tsx
@@ -5,61 +5,74 @@ import { ObjetModel1 } from '../../assets/models/ObjetModel1'
 import { ObjetModel2 } from '../../assets/models/ObjetModel2'
 import { ObjetModel3 } from '../../assets/models/ObjetModel3'
 import { useRef, useMemo, useEffect } from 'react'
+import NoDataLottie from '../../components/lotties/NoDataLottie'
+import { NoDataContainer, InnerText, GoObjetButton } from './LoungeStyles'
+import { useNavigate } from 'react-router-dom'
+import { URL } from '../../static'
 
 interface RandomModelsProps {
   onModelClick: (model: THREE.Group) => void
+  objets?: Objet[]
 }
 
 interface ObjetsProps {
   type: string
+  objets?: Objet[]
 }
 
-function RandomModels({ onModelClick }: RandomModelsProps) {
+interface Objet {
+  objet_id: number
+  type: string
+  name: string
+}
+
+function ObjetModels({ objets, onModelClick }: RandomModelsProps) {
   const groupRef = useRef<THREE.Group>(null)
 
   const models = useMemo(() => {
     const availableModels = [ObjetModel1, ObjetModel2, ObjetModel3]
-    const generatedModels = []
+    const generatedModels: any[] = []
 
-    for (let i = 0; i < 200; i++) {
-      const num = Math.floor(Math.random() * availableModels.length)
-      const ModelComponent = availableModels[num]
-      const mesh = new THREE.Group()
-      mesh.position.set(
-        Math.random() * 600 - 300,
-        Math.random() * 600 - 300,
-        Math.random() * 600 - 300
-      )
-      mesh.rotation.set(
-        Math.random() * 2 * Math.PI,
-        Math.random() * 2 * Math.PI,
-        Math.random() * 2 * Math.PI
-      )
+    if (objets && objets.length > 0) {
+      objets.forEach((objet) => {
+        const num = Math.floor(Math.random() * availableModels.length)
+        const ModelComponent = availableModels[num]
+        const mesh = new THREE.Group()
+        mesh.position.set(
+          Math.random() * 600 - 300,
+          Math.random() * 600 - 300,
+          Math.random() * 600 - 300
+        )
+        mesh.rotation.set(
+          Math.random() * 2 * Math.PI,
+          Math.random() * 2 * Math.PI,
+          Math.random() * 2 * Math.PI
+        )
 
-      switch (num) {
-        case 0:
-          mesh.scale.set(19.2, 19.2, 19.2)
-          break
-        case 1:
-          mesh.scale.set(3, 3, 3)
-          break
-        case 2:
-          mesh.scale.set(0.258, 0.258, 0.258)
-          break
-      }
-      mesh.userData = { onClick: () => onModelClick(mesh) }
-      generatedModels.push({ ModelComponent, mesh })
+        switch (objet.type) {
+          case 'O0001':
+            mesh.scale.set(19.2, 19.2, 19.2)
+            break
+          case 'O0002':
+            mesh.scale.set(3, 3, 3)
+            break
+          case 'O0003':
+            mesh.scale.set(0.258, 0.258, 0.258)
+            break
+        }
+        mesh.userData = { onClick: () => onModelClick(mesh) }
+        generatedModels.push({ ModelComponent, mesh })
+      })
     }
 
     return generatedModels
-  }, [onModelClick])
+  }, [objets, onModelClick])
 
   useEffect(() => {
     const group = groupRef.current
     if (group) {
-      models.forEach(({ ModelComponent, mesh }) => {
+      models.forEach(({ mesh }) => {
         group.add(mesh)
-        ;<ModelComponent />
       })
     }
   }, [models])
@@ -80,7 +93,7 @@ function RandomModels({ onModelClick }: RandomModelsProps) {
   )
 }
 
-function LoungeObjets() {
+function LoungeObjets({ objets }: { objets?: Objet[] }) {
   const { camera, gl, scene } = useThree()
   const controlsRef = useRef<any>(null)
   const refGroup = useRef<THREE.Group>(null)
@@ -129,17 +142,37 @@ function LoungeObjets() {
       <ambientLight intensity={3} />
       <directionalLight position={[2, 1, 3]} intensity={1} />
       <group ref={refGroup}>
-        <RandomModels onModelClick={handleModelClick} />
+        <ObjetModels objets={objets} onModelClick={handleModelClick} />
       </group>
     </>
   )
 }
 
-export default function App({ type }: ObjetsProps) {
+export default function App({ type, objets }: ObjetsProps) {
+  const navigate = useNavigate()
+
+  const handleClickGoObjet = () => {
+    navigate(URL.newObjet)
+  }
+
+  if (objets?.length === 0) {
+    return (
+      <NoDataContainer>
+        <NoDataLottie />
+        <InnerText>
+          <span>오브제가 없습니다 🥲</span>
+          <GoObjetButton onClick={handleClickGoObjet}>
+            오브제 생성하러 가기
+          </GoObjetButton>
+        </InnerText>
+      </NoDataContainer>
+    )
+  }
+
   return (
     type === 'lounge' && (
       <Canvas>
-        <LoungeObjets />
+        <LoungeObjets objets={objets} />
       </Canvas>
     )
   )
