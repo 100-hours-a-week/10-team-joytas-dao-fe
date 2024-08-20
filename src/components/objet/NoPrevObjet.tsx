f6cdced0745215456786f2d6eacb7088d52fbcfe
erica0321 <hyunahrho29@gmail.com>
2024-08-20 22:06:41 +0900

    feat: 메인 페이지 오브제 미리보기 API 연결

---
+++ src/components/objet/NoPrevObjet.tsx
@@ -0,0 +1,13 @@
+import { OuterContainer, InnerText, GoLoungeButton } from './NoPrevObjetStyle'
+
+export default function NoPrevObjet() {
+  return (
+    <OuterContainer>
+      <InnerText>
+        <p>앗! 오브제가 없어요 😭</p>
+        <p>오브제를 만들어보세요!</p>
+      </InnerText>
+      <GoLoungeButton>만들러 가기</GoLoungeButton>
+    </OuterContainer>
+  )
+}
