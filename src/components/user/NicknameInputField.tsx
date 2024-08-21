e5f9ca2d9d96b5af086d86f910f39494515dab48
erica0321 <hyunahrho29@gmail.com>
2024-08-21 16:20:31 +0900

    feat: 프로필 이미지, 닉네임 컴포넌트 추가

---
+++ src/components/user/NicknameInputField.tsx
@@ -0,0 +1,44 @@
+import { ChangeEvent } from 'react'
+import {
+  NicknameContainer,
+  NicknameInput,
+  NicknameTitle,
+} from '../../pages/user/ProfileStyles'
+import { RedTextLong } from '../../pages/lounge/LoungeStyles'
+import { RedText } from '../../pages/objet/ObjetStyles'
+
+interface NicknameInputFieldProps {
+  nickname: string
+  nicknameError: string
+  setNickname: (nickname: string) => void
+  validateNickname: (nickname: string) => Promise<boolean>
+}
+
+export default function NicknameInputField({
+  nickname,
+  nicknameError,
+  setNickname,
+  validateNickname,
+}: NicknameInputFieldProps) {
+  const handleNicknameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newNickname = event.target.value
+    setNickname(newNickname)
+    validateNickname(newNickname)
+  }
+
+  return (
+    <NicknameContainer>
+      <NicknameTitle>
+        닉네임<RedText>*</RedText>
+      </NicknameTitle>
+      <NicknameInput
+        placeholder='닉네임을 입력해주세요'
+        minLength={2}
+        maxLength={10}
+        value={nickname}
+        onChange={handleNicknameChange}
+      />
+      <RedTextLong style={{ marginLeft: '10px' }}>{nicknameError}</RedTextLong>
+    </NicknameContainer>
+  )
+}
---
+++ src/components/user/ProfileImageUploader.tsx
@@ -0,0 +1,78 @@
+import { ChangeEvent } from 'react'
+import {
+  ProfileContainer,
+  ImageContainer,
+  Profile,
+  ModifyButton,
+  ProfileTitle,
+  NoProfile,
+} from '../../pages/user/ProfileStyles'
+import { RedTextLong } from '../../pages/lounge/LoungeStyles'
+import { RedText } from '../../pages/objet/ObjetStyles'
+
+interface ProfileImageUploaderProps {
+  profile: File | null
+  profileUrl: string
+  imageError: string
+  setProfile: (image: File) => void
+  setProfileUrl: (image: string) => void
+  setImageError: (error: string) => void
+}
+
+export default function ProfileImageUploader({
+  profileUrl,
+  imageError,
+  setProfile,
+  setProfileUrl,
+  setImageError,
+}: ProfileImageUploaderProps) {
+  const handleChangeProfileImage = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+
+    if (file) {
+      // 파일 크기 체크 (1MB = 1 * 1024 * 1024 bytes)
+      const maxSizeInMB = 1
+      if (file.size > maxSizeInMB * 1024 * 1024) {
+        setImageError('파일 크기가 1MB를 초과할 수 없습니다.')
+        return
+      }
+
+      setProfile(file)
+
+      const reader = new FileReader()
+      reader.onload = (data) => {
+        if (data.target?.result) {
+          setProfileUrl(data.target.result as string)
+          setImageError('')
+        }
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+
+  return (
+    <ProfileContainer>
+      <ProfileTitle>
+        프로필 이미지<RedText>*</RedText>
+      </ProfileTitle>
+      <ImageContainer>
+        {profileUrl ? (
+          <Profile alt='profile' src={profileUrl} />
+        ) : (
+          <NoProfile />
+        )}
+        <div>
+          <ModifyButton htmlFor='imageInput'>변경</ModifyButton>
+          <input
+            id='imageInput'
+            onChange={handleChangeProfileImage}
+            type='file'
+            accept='image/*'
+            style={{ display: 'none' }}
+          />
+        </div>
+      </ImageContainer>
+      <RedTextLong>{imageError}</RedTextLong>
+    </ProfileContainer>
+  )
+}
