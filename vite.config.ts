94bd55f0ddb576635b467e70872f9219e5ab39ca
erica0321 <hyunahrho29@gmail.com>
2024-08-25 19:49:36 +0900

    feat: sentry 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "erica-z3",
+    project: "dao-fe"
+  })],
+
+  build: {
+    sourcemap: true
+  }
 })
