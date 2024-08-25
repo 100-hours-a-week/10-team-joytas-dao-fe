2236d7f04458c172e1a00e94d89db008a38b3a85
erica0321 <hyunahrho29@gmail.com>
2024-08-25 19:49:36 +0900

    feat: sentry 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,29 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import * as Sentry from '@sentry/react'
 
 const queryClient = new QueryClient()
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [
+    'localhost',
+    /^https:\/\/joytas\.kro\.kr/,
+    /^https:\/\/joytas\.kro\.kr\/api/,
+    /^https:\/\/api\.joytas\.kro\.kr/,
+  ],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
