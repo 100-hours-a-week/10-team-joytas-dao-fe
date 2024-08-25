d7eb34df14bd94eb252e3f862f58b6ddbdc2736d
erica0321 <hyunahrho29@gmail.com>
2024-08-25 22:19:00 +0900

    feat: 유저 정보 훅 추가

---
+++ src/hooks/useInfo.ts
@@ -0,0 +1,73 @@
+import { useNavigate } from 'react-router-dom'
+import type { Profile } from '../types/ProfileType'
+import useUserStore from '../store/userStore'
+import { APIs, URL } from '../static'
+
+export const useUserInfo = () => {
+  const navigate = useNavigate()
+
+  const { updateId, updateNickname, updateProfileImage } = useUserStore()
+
+  const fetchProfile = async (): Promise<Profile | undefined> => {
+    try {
+      let response = await fetch(APIs.profile, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+        },
+      })
+
+      if (response.status === 401) {
+        const reissueResponse = await fetch(APIs.reissueToken, {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+
+        if (reissueResponse.ok) {
+          const reissueData = await reissueResponse.json()
+          localStorage.setItem('access_token', reissueData.data.access_token)
+
+          // 새로운 토큰으로 프로필 재요청
+          response = await fetch(APIs.profile, {
+            credentials: 'include',
+            headers: {
+              Authorization: `Bearer ${reissueData.data.access_token}`,
+            },
+          })
+        } else {
+          throw new Error('Failed to reissue token')
+        }
+      }
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch profile')
+      }
+
+      const responseData = await response.json()
+      return responseData.data
+    } catch (error) {
+      console.error('Failed to fetch profile', error)
+      navigate(URL.home)
+    }
+  }
+
+  const getProfile = async () => {
+    const profile = await fetchProfile()
+
+    if (profile?.user_status === 'ACTIVE_FIRST_LOGIN') {
+      navigate(URL.firstProfile)
+    }
+
+    if (profile) {
+      updateNickname(profile.nickname)
+      updateProfileImage(profile.profile_url)
+      updateId(profile.user_id)
+    } else {
+      navigate(URL.home)
+    }
+  }
+
+  return { getProfile }
+}
