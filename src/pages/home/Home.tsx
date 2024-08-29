9a440706ee4600abfdc3357b861e8f511dde9d64
erica0321 <hyunahrho29@gmail.com>
2024-08-29 14:18:40 +0900

    feat: 홈 광고 영역 추가

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,8 @@
 import Layout from '../../components/Layout'
 import { GloablContainer16 } from '../../global/globalStyles'
 import {
-  Banner,
+  BannerVideo,
+  BannerImage,
   MyObjetContainer,
   MyObjetTitle,
   LottieContainer,
@@ -11,6 +12,9 @@ import { useEffect, useState } from 'react'
 import { APIs } from '../../static'
 import LoadingLottie from '../../components/lotties/LoadingLottie'
 import NoPrevObjet from '../../components/objet/NoPrevObjet'
+import { Carousel } from 'antd'
+import banner1 from '../../assets/images/banner1.png'
+import banner2 from '../../assets/images/banner2.png'
 
 export default function Home() {
   const [objets, setObjets] = useState([])
@@ -45,13 +49,17 @@ export default function Home() {
   return (
     <Layout style={{ padding: '0px' }}>
       <GloablContainer16 style={{ padding: '0px' }}>
-        <Banner
-          autoPlay
-          muted
-          loop
-          playsInline
-          src='https://oopy.lazyrockets.com/api/v2/notion/fileUrl?src=https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F38552da6-340d-42c1-a9a1-b181ff331f03%2F64583d5a-5694-4c0e-9180-31e58fdd001a%2F%25E1%2584%258C%25E1%2585%25A6%25E1%2584%258C%25E1%2585%25AE_(PC).mp4&blockId=8e6adf11-495d-4115-ba04-12f87e247b9f#t=0.0001'
-        ></Banner>
+        <Carousel autoplay autoplaySpeed={2500}>
+          <BannerImage src={banner2} />
+          <BannerImage src={banner1} />
+          <BannerVideo
+            autoPlay
+            muted
+            loop
+            playsInline
+            src='https://oopy.lazyrockets.com/api/v2/notion/fileUrl?src=https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F38552da6-340d-42c1-a9a1-b181ff331f03%2F64583d5a-5694-4c0e-9180-31e58fdd001a%2F%25E1%2584%258C%25E1%2585%25A6%25E1%2584%258C%25E1%2585%25AE_(PC).mp4&blockId=8e6adf11-495d-4115-ba04-12f87e247b9f#t=0.0001'
+          ></BannerVideo>
+        </Carousel>
 
         <MyObjetContainer>
           <MyObjetTitle>👀 최근 오브제를 확인해보세요!</MyObjetTitle>
