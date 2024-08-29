12c3d94fabd883b6ba0d3089019a9c827e35b727
erica0321 <hyunahrho29@gmail.com>
2024-08-29 14:18:48 +0900

    fix: 빌드 실패 수정

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -174,7 +174,7 @@ export function LoungeListModal({
 export function ConfirmNotificationModal({
   onClose,
   isLoading,
-  handleConfirm,
+  // handleConfirm,
 }: NotiProps) {
   return (
     <DeleteModalContainer>
