10edf9de544c270d0d68113f6a8ef8403d6a9f37
erica0321 <hyunahrho29@gmail.com>
2024-08-31 17:03:30 +0900

    feat: 콕 찌르기 추가

--- src/components/notification/NotificationItem.tsx
+++ src/components/notification/NotificationItem.tsx
@@ -7,11 +7,13 @@ import {
 } from '../../pages/notification/NotificationStyles'
 import { extractTime } from '../../utils/formatDatetime'
 import TagImg from '../../assets/images/tag.png'
+import PokeImg from '../../assets/images/poke.png'
 import InviteImg from '../../assets/images/loungeInvite.png'
 import { ModalBackdrop } from '../modal/ModalStyles'
 import { useState } from 'react'
 import { ConfirmNotificationModal } from '../modal/Modal'
-import { APIs } from '../../static'
+import { useNavigate } from 'react-router-dom'
+import { APIs, URL } from '../../static'
 
 export default function NotificationItem({
   notification_id,
@@ -26,10 +28,11 @@ export default function NotificationItem({
   } = {
     N0001: InviteImg, // 라운지 초대
     N0002: TagImg, // 오브제 맴버 태그
-    poke: TagImg,
+    N0003: PokeImg, // 콕 찌르기
     voice: TagImg,
   }
   const [isModalVisible, setIsModalVisible] = useState(false)
+  const navigate = useNavigate()
 
   let text = ''
   switch (type) {
@@ -39,36 +42,65 @@ export default function NotificationItem({
     case 'N0002':
       text = `${sender.nickname}님이 ${detail.name} 오브제에 태그했습니다.`
       break
+    case 'N0003':
+      text = `${sender.nickname}님이 콕 찔렀습니다.`
   }
 
-  const handleClickNoti = async () => {
-    setIsModalVisible(true)
-
-    setTimeout(async () => {
-      try {
-        const readRes = await fetch(`${APIs.notification}/${notification_id}`, {
+  const handleClickNotiRead = async () => {
+    try {
+      const readRes = await fetch(
+        `${APIs.notification}/${notification_id}/read`,
+        {
           method: 'PATCH',
+          credentials: 'include',
           headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${localStorage.getItem('access_token')}`,
           },
-        })
-
-        if (readRes.ok) {
-          console.log('알림 클릭 성공')
         }
-      } catch (error) {
-        console.error('알림 클릭 실패', error)
+      )
+
+      if (readRes.ok) {
+        console.log('알림 클릭 성공')
       }
+    } catch (error) {
+      console.error('알림 클릭 실패', error)
+    }
+  }
+
+  const handleClickLoungeNoti = async () => {
+    setIsModalVisible(true)
+
+    setTimeout(async () => {
+      await handleClickNotiRead()
       setIsModalVisible(false)
     }, 3000)
+  }
+
+  const handleClickObjetNoti = async () => {
+    await handleClickNotiRead()
+  }
+
+  const handleClickPokeNoti = async () => {
+    await handleClickNotiRead()
+    console.log(sender.user_id)
+    navigate(`${URL.userDetail}/${sender.user_id}`)
   }
 
   return (
     <>
       {isModalVisible && <ModalBackdrop />}
 
-      <NotificationItemContainer onClick={handleClickNoti} $isRead={is_read}>
+      <NotificationItemContainer
+        onClick={() => {
+          type === 'N0001'
+            ? handleClickLoungeNoti()
+            : type === 'N0002'
+              ? handleClickObjetNoti()
+              : handleClickPokeNoti()
+        }}
+        $isRead={is_read}
+      >
         <TypeImg src={typeEmoji[type]} />
         <NotiContents>{text}</NotiContents>
         <NotiDatetime>{extractTime(datetime)}</NotiDatetime>
