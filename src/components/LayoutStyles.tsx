710513be720d1445ab0049fbbf193593045ec1d1
erica0321 <hyunahrho29@gmail.com>
2024-09-02 15:56:11 +0900

    fix: 스타일 수정

--- src/components/LayoutStyles.tsx
+++ src/components/LayoutStyles.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 export const Main = styled.div`
   width: 100%;
   height: 100%;
-  border: 0.1px solid rgba(255, 255, 255, 0.8);
 `
 
 export const HeaderContainer = styled.div`
