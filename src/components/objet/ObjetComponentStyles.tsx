b4b9abd16121634fe18fe4ef76a66a042f345733
yoouung <jyp101400@gmail.com>
2024-09-05 01:22:32 +0900

    design: 메인페이지 디자인 수정

--- src/components/objet/ObjetComponentStyles.tsx
+++ src/components/objet/ObjetComponentStyles.tsx
@@ -3,32 +3,47 @@ import styled from 'styled-components'
 export const ObjetList = styled.div`
   width: 100%;
   padding: 0 32px;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  gap: 10px;
+  margin-bottom: 20px;
+
+  @media (min-width: 600px) {
+    grid-template-columns: repeat(2, 1fr);
+    gap: 15px;
+  }
 `
 
 export const ObjetContainer = styled.div`
   cursor: pointer;
   width: 100%;
   display: flex;
+  flex-direction: row;
   align-items: flex-start;
-  gap: 10px;
+  gap: 20px;
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  padding: 8px;
 
   &:hover {
     transform: scale(1.05);
   }
-`
-export const ObjetImage = styled.img`
-  width: 80px;
-  height: 80px;
-  object-fit: cover;
-  border-radius: 10px;
-  background-color: grey;
 
   @media (min-width: 600px) {
-    width: 100px;
-    height: 100px;
+    flex-direction: column;
+  }
+`
+export const ObjetImage = styled.img`
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border-radius: 10px;
+  background-color: black;
+  align-self: center;
+
+  @media (min-width: 600px) {
+    width: 200px;
+    height: 200px;
   }
 `
 
@@ -47,19 +62,19 @@ export const ObjetContent = styled.div`
 
 export const ObjetTitle = styled.div`
   margin-top: 8px;
-  font-size: 15px;
-  font-weight: bold;
+  font-size: 13px;
+  font-weight: 500;
 
   @media (min-width: 600px) {
-    font-size: 20px;
+    font-size: 15px;
   }
 `
 
 export const ObjetDescription = styled.div`
   color: rgba(255, 255, 255, 0.7);
-  font-size: 10px;
+  font-size: 9px;
   @media (min-width: 600px) {
-    font-size: 16px;
+    font-size: 12px;
   }
 `
 
