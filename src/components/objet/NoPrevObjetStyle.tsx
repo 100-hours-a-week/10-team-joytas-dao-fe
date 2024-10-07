37bf22dc03c7fdae6613ecfa0216afb3c73791f3
yoouung <jyp101400@gmail.com>
2024-10-07 16:40:40 +0900

    design: 메인페이지 레이아웃 수정

--- src/components/objet/NoPrevObjetStyle.tsx
+++ src/components/objet/NoPrevObjetStyle.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const OuterContainer = styled.div`
   width: 100%;
-  height: 200px;
+  height: 290px;
   display: flex;
   align-items: center;
   justify-content: center;
